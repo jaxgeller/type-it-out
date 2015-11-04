786db5cc2c510656913197a960778727f5a864e1
jaxgeller <jacksongeller@gmail.com>
2015-11-04 11:44:11 -0500

    initial commit

---
+++ src/inject/inject.js
@@ -0,0 +1,16 @@
+'use strict';
+
+var div = document.createElement('div');
+  div.classList.add('_tio-message');
+  div.innerHTML = '<p>Type it out</p>';
+
+document.body.appendChild(div);
+
+document.oncopy = function(e) {
+  window.getSelection().empty();
+  div.classList.add('_tio-animate');
+
+  setTimeout(function() {
+    div.classList.remove('_tio-animate');
+  }, 1000);
+}
